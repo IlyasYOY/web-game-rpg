4f8d995c4e27c119d47de4fa0001dd736dcffa6c
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-11-14 21:57:31 +0300

    Events added.

--- src/common/js/Player.js
+++ src/common/js/Player.js
@@ -1,64 +1,66 @@
-var allSkills = ['fireball','heal','kick','armor'];
+var allSkills = ['fireball', 'heal', 'kick', 'armor'];
 
 typesOfUnit = {
-    'magician' : {
-        health : 80,
-        power : 5,
-        speed : 10,
-        luck : 15,
-        skills : {'fireball' : 10,
-            'heal' : 5}
-    },
-    'warrior' : {
-        health : 100,
-        power : 10,
-        speed : 5,
-        luck : 10,
-        skills : {
-            'kick' : 10,
-            'armor' : 5
+    'magician': {
+        health: 80,
+        power: 5,
+        speed: 10,
+        luck: 15,
+        skills: {
+            'fireball': 10,
+            'heal': 5
         }
     },
-    'archer' : {
-        power : 7,
-        speed : 15,
-        luck : 12,
-        skills : {}
+    'warrior': {
+        health: 100,
+        power: 10,
+        speed: 5,
+        luck: 10,
+        skills: {
+            'kick': 10,
+            'armor': 5
+        }
+    },
+    'archer': {
+        power: 7,
+        speed: 15,
+        luck: 12,
+        skills: {}
     }
 }
 
-class Entity{
-    constructor(x = 0,y = 0,direction = 0,typeOfUnit = "magician"){
+class Entity {
+    constructor(x = 0, y = 0, direction = 0, typeOfUnit = "magician") {
         this.x = x;
         this.y = y;
         this.direction = direction;
         this.typeOfunit = typeOfUnit;
     }
 
-    getX(){
+    getX() {
         return this.x;
     }
 
-    getY(){
+    getY() {
         return this.y;
     }
 
-    setX(value){
+    setX(value) {
         this.x = value;
     }
 
-    setY(value){
+    setY(value) {
         this.y = value;
     }
 
-    getDirection(){
+    getDirection() {
         return this.direction;
     }
 }
 
-class Player extends Entity{
-    constructor(x = 0,y = 0,direction = 0, typeOfUnit = 'magician', energy = 10){
-        super(x,y,direction);
+class Player extends Entity {
+    constructor(x = 0, y = 0, direction = 0, typeOfUnit = 'magician', energy = 10) {
+        super(x, y, direction);
         this.inventory = {};
         this.unit = typesOfUnit[typeOfUnit];
         this.typeOfUnit = typeOfUnit;
@@ -67,10 +69,12 @@ class Player extends Entity{
         this.skills = typesOfUnit[typeOfUnit].skills;
     }
 
-    move(newX,newY){
+    move(newX, newY) {
         this.x = newX;
         this.y = newY;
     }
-
-
 }
+
+module.exports.Entity = Entity;
+module.exports.Player = Player;
+module.exports.typesOfUnit = typesOfUnit;
