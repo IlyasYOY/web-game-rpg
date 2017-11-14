083fc2344784c572c688c740d1e4e5ffd20df11f
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-11-14 15:38:59 +0300

    some changes to server.js

--- src/server/js/server.js
+++ src/server/js/server.js
@@ -1,25 +1,31 @@
-express = require('express');
-app = express();
-http = require('http').Server(app);
-io = require('socket.io')(http);
-fs = require('fs');
+let fs = require('fs');
+let path = require('path');
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
-port = 8080;
-ip = '127.0.0.1';
+let port = process.env.PORT || 8080;
+let ip = "127.0.0.1";
 
-module.exports = function startServer(path) {
-
-    app.use(express.static(path + '/src/'));
-
-    app.get('/', function (req, res) {
-        res.sendFile(path + '/src/client/html/index.html');
+module.exports = function startServer(dir) {
+    app.use(express.static(path.join(dir, "/src")));
+    app.get("/", function (req, res, next) {
+        res.sendFile(path.join(dir, "/src/client/html/index.html"));
     });
 
-    io.on("connection", function (socket) {
+    io.on("connect", function (socket) {
         console.log(`Socket connected: ${socket.id}`);
+        configureSocket(socket)
     });
 
     http.listen(port, function () {
         console.log(`Server works on http://${ip}:${port}/`);
     });
 };
+
+function configureSocket(socket) {
+    socket.on("disconnect", function (socket) {
+        console.log("Socket disconnected.");
+    });
+}
