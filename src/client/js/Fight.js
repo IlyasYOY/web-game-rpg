d87b2514b06ba242e6b02f63dec38f3ec828c92e
kruglik <kruglikalexey@ya.ru>
2017-11-18 00:18:35 +0300

    start fight logic

--- src/client/js/Fight.js
+++ src/client/js/Fight.js
@@ -42,33 +42,79 @@ var cuHandler = function (me) {
 var drawWarrior = function(positionX,positionY){
     //head
     canvasContext.fillStyle = '#ffe789';
-    canvasContext.fillRect(50 + positionX,0 + positionY,50,50);
+    canvasContext.fillRect(25 + positionX,0 + positionY,25,25);
     //body
     canvasContext.fillStyle = '#ff2b23';
-    canvasContext.fillRect(25 + positionX,50 + positionY,100,100);
+    canvasContext.fillRect(12.5 + positionX,25 + positionY,50,50);
     //left arm
     canvasContext.fillStyle = '#ffe789';
-    canvasContext.fillRect(0 + positionX,50 + positionY,25,50);
+    canvasContext.fillRect(0 + positionX,25 + positionY,12.5,25);
     //right arm
     canvasContext.fillStyle = '#ffe789';
-    canvasContext.fillRect(125 + positionX,50 + positionY,25,50);
+    canvasContext.fillRect(62.5 + positionX,25 + positionY,12.5,25);
     //left foot
     canvasContext.fillStyle = '#0f0aff';
-    canvasContext.fillRect(30 + positionX,150 + positionY,25,50);
+    canvasContext.fillRect(15 + positionX,75 + positionY,12.5,25);
     //right foot
     canvasContext.fillStyle = '#0f0aff';
-    canvasContext.fillRect(90 + positionX,150 + positionY,25,50);
+    canvasContext.fillRect(45 + positionX,75 + positionY,12.5,25);
 };
 
-var writeStat = function (me,enemy) {
-    canvasContext.font="20px Georgia";
-    canvasContext.fillText('You : ',0,20);
-    canvasContext.fillText('health = ' + players[me].health, 60,20);
-    canvasContext.fillText('energy = ' + players[me].energy, 200,20);
-    canvasContext.fillText('VS',0,40);
-    canvasContext.fillText('Player #' + enemy + ' : ',0,60);
-    canvasContext.fillText('health = ' + players[enemy].health, 100,60);
-    canvasContext.fillText('energy = ' + players[enemy].energy, 240,60);
+var drawMagician = function(positionX,positionY,number){
+    canvasContext.font="14px Georgia";
+    canvasContext.fillStyle = '#000000';
+    canvasContext.fillText(number, 15 + positionX, 5 + positionY);
+    //hat
+    canvasContext.fillStyle = '#000000';
+    canvasContext.fillRect(20 + positionX,5 + positionY,35,5);
+    canvasContext.fillRect(25 + positionX,10 + positionY,25,10);
+    //head
+    canvasContext.fillStyle = '#ffe789';
+    canvasContext.fillRect(25 + positionX,20 + positionY,25,25);
+    //body
+    canvasContext.fillStyle = '#1217ff';
+    canvasContext.fillRect(12.5 + positionX,30 + positionY,50,60);
+    //left arm
+    canvasContext.fillStyle = '#ffe789';
+    canvasContext.fillRect(0 + positionX,30 + positionY,12.5,25);
+    //right arm
+    canvasContext.fillStyle = '#ffe789';
+    canvasContext.fillRect(62.5 + positionX,30 + positionY,12.5,25);
+};
+
+var writeStat = function (myEnemy) {
+    canvasContext.font="14px Georgia";
+    canvasContext.fillStyle = '#000000';
+    canvasContext.fillText('You : ', 0, 14);
+
+    let health = 0;
+    for (let i in myPerson.units){
+        health += myPerson.units[i] * typesOfUnit[i].health;
+    }
+    canvasContext.fillText('health = ' + health, 0, 28);
+    canvasContext.fillText('energy = ' + myPerson.energy, 0, 42);
+    canvasContext.fillText('VS',0,56);
+    canvasContext.fillText('Player #' + myEnemy + ' : ', 0, 70);
+    console.log('myEnemy' + players[myEnemy]);
+    health = 0;
+    for (let i in players[myEnemy].units){
+        health += players[myEnemy].units[i] * typesOfUnit[i].health;
+    }
+    canvasContext.fillText('health = ' + health, 0, 84);
+    canvasContext.fillText('energy = ' + players[myEnemy].energy, 0, 98);
+};
+
+var drawMyPerson = function () {
+    let health = 0;
+    for (let i in myPerson.units){
+        if (i === 'warrior'){
+            drawWarrior(50,250,myPerson.units[i]);
+        } else if(i === 'magician'){
+            drawMagician(50,375,myPerson.units[i]);
+        }
+    }
+
+
 };
 
 
@@ -88,14 +134,12 @@ var drawSkills = function (me) {
     }
 };
 
-var fightHandler = function (me,enemy) {
+var fightHandler = function (myEnemy) {
     whereAmI = 'Fight';
-    stroke = me;
-
-    writeStat(me,enemy);
-    drawWarrior(50,250);
-    drawWarrior(400,250);
-    drawSkills(stroke);
-    cuHandler(me);
-    doAttack();
+    writeStat(myEnemy);
+    drawMyPerson();
+    // drawWarrior(400,250);
+    // drawSkills(stroke);
+    // cuHandler(me);
+    // doAttack();
 };
