16d4d564142b7ff0c90e8e0c1e5f4edf4feab7e6
kruglik <kruglikalexey@ya.ru>
2017-11-21 03:28:00 +0300

    map menu added

---
+++ src/client/js/MapsMenu.js
@@ -0,0 +1,34 @@
+var MapMenu = {
+    menuPoints: ['map1', 'map2', 'map3'],
+    numberOfPoint: 0
+};
+
+var printMapMenu = function(chainedPoint){
+    canvasContext.font="20px Georgia";
+    canvasContext.fillStyle = "#000000";
+    var stepInMenu = canvasHeight / 30;
+
+    for (var i = 0;i<MapMenu.menuPoints.length;++i){
+        canvasContext.fillText(MapMenu.menuPoints[i],((canvasHeight - stepInMenu) / 2),((canvasWidth-stepInMenu*(MapMenu.menuPoints.length)) / 2) +  i*stepInMenu);
+    }
+
+    canvasContext.fillRect(((canvasHeight - stepInMenu) / 2) - 2*stepInMenu,
+        ((canvasWidth-stepInMenu*(MapMenu.menuPoints.length)) / 2) +  (chainedPoint-1)*stepInMenu,20,20);
+
+};
+
+var mapMenuHandler = function () {
+    if (keysPushed[ButtonsKeys['up']] === true){
+        if (MapMenu.numberOfPoint>0) --MapMenu.numberOfPoint;
+    }
+
+    if (keysPushed[ButtonsKeys['down']] === true){
+        if ((MapMenu.numberOfPoint+1)<MapMenu.menuPoints.length) ++MapMenu.numberOfPoint;
+    }
+
+    if (keysPushed[ButtonsKeys['enter']] === true){
+        socket.emit("chose_map",MapMenu.menuPoints[MapMenu.numberOfPoint]);
+    }
+
+    printMapMenu(MapMenu.numberOfPoint);
+};
